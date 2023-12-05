72ef20204a78d9d314b86f132614579035a1e64a
Steven <steven.ayoub.362@my.csun.edu>
2023-12-05 14:37:32 -0800

    edit style of calculate_distance function

--- molecool/functions.py
+++ molecool/functions.py
@@ -56,10 +56,29 @@ def zen(with_attribution=True):
     return quote
 
 def calculate_distance(rA, rB):
-    # This function calculates the distance between two points given as numpy arrays.
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
-    return dist
+    """Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    distance : float
+        The distance between the two points.
+    
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 0.1, 0])
+    >>> calculate_distance(r1, r2)
+    0.1
+    """
+    dist_vec = (rA - rB)
+    distance = np.linalg.norm(dist_vec)
+        
+    return distance
 
 def open_pdb(file_location):
     
